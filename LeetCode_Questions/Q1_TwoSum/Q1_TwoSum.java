fd7b857e8154c56a3c8e925ea31ec82c5b8f20d0
Soumik Pal <soumikpal633@gmail.com>
2026-09-05 12:01:38 +0530

    Q1_TwoSum Solution

---
+++ LeetCode_Questions/Q1_TwoSum/Q1_TwoSum.java
@@ -0,0 +1,31 @@
+class Q1_TwoSum {
+
+    
+    public int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+
+        return new int[] {};
+    }
+
+    // Main method for VS Code
+    //Taking input in IDE
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        Q1_TwoSum obj = new Q1_TwoSum();
+
+        int[] result = obj.twoSum(nums, target);
+
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+    }
+}
